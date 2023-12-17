7a5cc69bbdbeb24677b795042aca1a063425e7fe
W192547975 <W192547975@163.com>
2023-12-17 09:37:37 +0800

    Update copyright

--- list2/list2.h
+++ list2/list2.h
@@ -1,6 +1,5 @@
-//This work is licensed under Attribution 4.0 International.
-//To view a copy of this license,
-//visit http://creativecommons.org/licenses/by/4.0/
+﻿//This work © 2023 by W192547975 is licensed under Attribution 4.0 International.
+//To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
 
 /*
 Remember:
