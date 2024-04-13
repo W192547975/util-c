c9639bcae586c2f73e98a6ab6276c69500e79588
W192547975 <W192547975@163.com>
2024-04-13 16:23:52 +0800

    Update copyrights

--- list/list.c
+++ list/list.c
@@ -1,5 +1,17 @@
-//This work © 2022 by W192547975 is licensed under Attribution 4.0 International.
-//To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
+/* Copyright 2022 W192547975
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
 
 #if 0
 #error 把编译器写废了
