0376d66b52970278dfdae8b74da6bd1b68041e3b
W192547975 <W192547975@163.com>
2024-04-13 20:19:50 +0800

    Update .h files
    Remove BOM in files.

--- list/list.h
+++ list/list.h
@@ -1,4 +1,4 @@
-﻿/* Copyright 2022 W192547975
+/* Copyright 2022 W192547975
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
--- list2/list2.h
+++ list2/list2.h
@@ -1,4 +1,4 @@
-﻿/* Copyright 2023 W192547975
+/* Copyright 2023 W192547975
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
